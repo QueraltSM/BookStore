448c7c0c1347d3b5602919d23a581166aa21b2c2
QueraltSosaMompel <queraltsosa@hotmail.com>
2018-12-25 10:57:59 +0000

    The book's information can be stored in the Firebase Database Realtime

---
+++ BookStore/MainVC.swift
@@ -0,0 +1,121 @@
+//
+//  MainVCViewController.swift
+//  BookStore
+//
+//  Created by Queralt Sosa Mompel on 23/12/18.
+//  Copyright © 2018 Queralt Sosa Mompel. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class MainVC: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var saveBook: UIButton!
+    @IBOutlet var bookImage: UIImageView!
+    @IBOutlet var bookTitle: UITextField!
+    @IBOutlet var bookAuthor: UITextField!
+    @IBOutlet var bookDate: UITextField!
+    let imagePicker = UIImagePickerController()
+    @IBOutlet var pickPhoto: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    func alert(titleParam : String, messageParam: String) {
+        let title = titleParam
+        let message = messageParam
+        let okText = "Ok"
+        let alert = UIAlertController(title: title, message : message, preferredStyle: UIAlertControllerStyle.alert)
+        let okayButton = UIAlertAction(title: okText, style: UIAlertActionStyle.cancel, handler: nil)
+        alert.addAction(okayButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func saveImage() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let storageRef = Storage.storage().reference().child("book_image_urls").child("\(uid).png")
+        guard let imageData = UIImageJPEGRepresentation(bookImage.image!, 0.75) else { return }
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        storageRef.putData(imageData, metadata: metaData) { metaData, error in
+            if error == nil, metaData != nil {
+                storageRef.downloadURL { url, error in
+                }
+            } else {
+                let title = "Failed to save photo"
+                self.alert(titleParam: title, messageParam : (error?.localizedDescription)!)
+            }
+        }
+    }
+    
+    func noCamera(){
+        let alertVC = UIAlertController(title: "No Camera",message: "Sorry, this device has no camera",preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",style:.default, handler: nil)
+        alertVC.addAction(okAction)
+        present(alertVC,animated: true,completion: nil)
+    }
+    
+    func shootPhoto () {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.cameraCaptureMode = .photo
+            imagePicker.modalPresentationStyle = .fullScreen
+            present(imagePicker,animated: true,completion: nil)
+        } else {
+            noCamera()
+        }
+    }
+    
+    
+    func photoFromGallery() {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        imagePicker.modalPresentationStyle = .popover
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func saveInfo(_ sender: Any) {
+        saveImage()
+        let ref = Database.database().reference()
+        ref.child("Title").setValue(bookTitle.text)
+        ref.child("Author").setValue(bookAuthor.text)
+        ref.child("Date").setValue(bookDate.text)
+    }
+    
+    
+    @IBAction func pickPhoto(_ sender: Any) {
+        let alertController = UIAlertController.init(title:"Select a photo", message: "", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction.init(title: "Gallery", style: .default, handler: { (action) in
+            self.photoFromGallery()
+        }))
+        alertController.addAction(UIAlertAction.init(title: "Camera", style: .default, handler: { (action) in
+            self.shootPhoto()
+        }))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info : [String:Any]) {
+        if let im = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            bookImage.image = im
+            bookImage.contentMode = .scaleAspectFit
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+}
---
+++ BookStore/Model/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  BookStore
+//
+//  Created by Queralt Sosa Mompel on 16/12/18.
+//  Copyright © 2018 Queralt Sosa Mompel. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    
+    
+    // método sort para poder ordenarlos en la lista
+    
+}
